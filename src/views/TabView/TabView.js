87d322455791bfd536be0797e05ea3869012bf33
Kevin Cooper <cooperka92@gmail.com>
2017-05-01 22:05:01 -0400

    Accept a 'tabBarOnPress' param

--- src/views/TabView/TabView.js
+++ src/views/TabView/TabView.js
@@ -19,6 +19,7 @@ export type TabViewConfig = {
   tabBarComponent?: ReactClass<*>,
   tabBarPosition?: 'top' | 'bottom',
   tabBarOptions?: {},
+  tabBarOnPress?: () => void,
   swipeEnabled?: boolean,
   animationEnabled?: boolean,
   lazy?: boolean,
@@ -35,6 +36,7 @@ type Props = {
   tabBarComponent?: ReactClass<*>,
   tabBarPosition?: 'top' | 'bottom',
   tabBarOptions?: {},
+  tabBarOnPress?: () => void,
   swipeEnabled?: boolean,
   animationEnabled?: boolean,
   lazy?: boolean,
@@ -121,6 +123,7 @@ class TabView extends PureComponent<void, Props, void> {
       <TabBarComponent
         {...props}
         {...tabBarOptions}
+        onPress={this.props.tabBarOnPress}
         navigation={this.props.navigation}
         getLabel={this._getLabel}
         renderIcon={this._renderIcon}
